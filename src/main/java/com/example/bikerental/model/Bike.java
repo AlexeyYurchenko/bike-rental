10dbe2c290d171650918145dee5a45fac41d9f23
admin <travers-2010@mail.ru>
2024-02-01 16:17:05 +0700

    Initial commit

---
+++ src/main/java/com/example/bikerental/model/Bike.java
@@ -0,0 +1,27 @@
+package com.example.bikerental.model;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bike {
+    private int id;
+
+    @NotEmpty(message = "Название велосипеда не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Название велосипеда должно быть от 2 до 100 символов длиной")
+    private String nameBike;
+
+    @NotEmpty(message = "Производители не должен быть пустым")
+    @Size(min = 2, max = 100, message = "Имя производителя должно быть от 2 до 100 символов длиной")
+    private String manufacturer;
+
+    @Min(value = 2010, message = "Год должен быть больше, чем 2010")
+    private int year;
+}
---
+++ src/main/java/com/example/bikerental/model/Person.java
@@ -0,0 +1,24 @@
+package com.example.bikerental.model;
+
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private int id;
+
+    @NotEmpty(message = "Имя не должно быть пустым")
+    @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
+    private String fullName;
+
+    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
+    private int yearOfBirth;
+
+}
